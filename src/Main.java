35d24031fdf89bb08fa9dcd3e755f473ffcc14c3
radams42 <radams42@uw.edu>
2023-10-28 22:06:39 -0700

    Work on GameFrame.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
-import java.awt.Frame;
 import javax.swing.SwingUtilities;
+import view.GameFrame;
 
 /**
- *
+ * Main driver for Trivia Maze.
+ * @author rick_adams.
+ * @author Viktoria Dolojan.
  */
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Frame().setVisible(true));
+    /**Private constructor.*/
+    private Main() {
+
+    }
+
+    /**
+     * Main method.
+     * @param thrArgs command line args.
+     */
+    public static void main(final String[] thrArgs) {
+        SwingUtilities.invokeLater(() -> {
+           final GameFrame gameFrame = new GameFrame();
+        });
     }
 }
--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -1,54 +1,86 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.List;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javax.swing.*;
 
 /**
- *
+ * Frame class for Trivia Maze.
+ * @author rick_adams.
+ * @author Viktoria Dolojan.
  */
-public class GameFrame extends JFrame {
+public class GameFrame extends JFrame implements PropertyChangeListener {
     private JFrame myGameFrame;
     private List myGameStats;
 
-    private static final Dimension DIMENSION = new Dimension(100, 100);
+    private static final Dimension DIMENSION = new Dimension(500, 500);
+
     
     public GameFrame() {
         super();
+        // Panel is for testing purposes only.
 
-        final JFrame frame = new JFrame();
-        
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menuFile = new JMenu("File");
-        JMenuItem menuItemExit = new JMenuItem("Exit");
-        menuFile.add(menuItemExit);
+        JPanel panel = new JPanel();
+        Dimension panelDim = new Dimension(100, 100);
+        panel.setPreferredSize(panelDim);
+        add(panel);
+        panel.setBackground(Color.PINK);
         frameHelper();
         setFocusable(true);
+        requestFocus();
         setVisible(true);
     }
 
     private void frameHelper() {
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         setSize(DIMENSION);
         setResizable(false);
+        setLocationRelativeTo(null);
         setTitle("Trivia Maze");
-        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public JMenuBar menuBarHelper(final JMenuBar theBar) {
-
-        return theBar;
+    public JMenuBar menuBarHelper() {
+        final JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu());
+        
+        return menuBar;
     }
     public void menuHelper() {
 
     }
 
+    private static JMenu fileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        final JMenuItem exit = new JMenuItem("Exit");
+
+        
+        exit.addActionListener(
+                e -> System.exit(0));
+        fileMenu.add(exit);
+        return fileMenu;
+    }
     public void saveAndLoad() {
 
     }
 
     public void gameDifficulty() {
+
+    }
+
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param theEvt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(final PropertyChangeEvent theEvt) {
 
     }
 }
