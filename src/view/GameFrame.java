b701ab8e55de8da0553001bd5f23ce78e5f0a997
radams42 <122660178+radams42@users.noreply.github.com>
2023-10-28 23:54:02 -0700

    GameFrame WIP.

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -12,7 +12,8 @@ import javax.swing.*;
  * @author rick_adams.
  * @author Viktoria Dolojan.
  */
-public class GameFrame extends JFrame implements PropertyChangeListener {
+public class
+GameFrame extends JFrame implements PropertyChangeListener {
     private JFrame myGameFrame;
     private List myGameStats;
 
@@ -22,47 +23,86 @@ public class GameFrame extends JFrame implements PropertyChangeListener {
     public GameFrame() {
         super();
         // Panel is for testing purposes only.
-
         JPanel panel = new JPanel();
         Dimension panelDim = new Dimension(100, 100);
         panel.setPreferredSize(panelDim);
         add(panel);
         panel.setBackground(Color.PINK);
         frameHelper();
+
         setFocusable(true);
         requestFocus();
         setVisible(true);
     }
 
     private void frameHelper() {
-
-
+        setTitle("Trivia Maze");
+        setJMenuBar(menuBarHelper());
         setSize(DIMENSION);
         setResizable(false);
         setLocationRelativeTo(null);
-        setTitle("Trivia Maze");
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public JMenuBar menuBarHelper() {
         final JMenuBar menuBar = new JMenuBar();
-        menuBar.add(fileMenu());
-        
-        return menuBar;
-    }
-    public void menuHelper() {
 
+        menuBar.add(fileMenu());
+        menuBar.add(infoMenu());
+
+
+        return menuBar;
     }
 
     private static JMenu fileMenu() {
         final JMenu fileMenu = new JMenu("File");
         final JMenuItem exit = new JMenuItem("Exit");
+        final JMenuItem start = new JMenuItem("Start");
+        final JMenuItem quit = new JMenuItem("Quit");
 
-        
+        fileMenu.add(start); //TODO: Install game commencement logic.
+        fileMenu.add(quit); //TODO: Install game end logic.
+
         exit.addActionListener(
                 e -> System.exit(0));
         fileMenu.add(exit);
         return fileMenu;
+    }
+
+    private static JMenu infoMenu() {
+        final JMenu infoMenu = new JMenu("Info");
+        final JMenuItem rules = new JMenuItem("Rules");
+        final JMenuItem help = new JMenuItem("Help");
+        final JMenuItem about = new JMenuItem("About Game");
+
+        rules.addActionListener(e -> JOptionPane.showMessageDialog(null,
+                """
+                        In order to advance to the next room, you must answer the given Trivia question.
+                        If you answer correctly,  the way forward may be reveled;
+                        If you answer incorrectly, the way forward may be forever shut.
+                        Answer wisely.\s"""));
+        infoMenu.add(rules);
+
+        help.addActionListener(e -> JOptionPane.showMessageDialog(null,
+                """
+                       There is no help for you, only pain and algorithms class.
+                       
+                       (This message is under further construction, and does not reflect thoughts,
+                       feelings and opinions of Team 2.
+                       Math is fun.)\s"""));
+        infoMenu.add(help);
+
+        about.addActionListener(e -> JOptionPane.showMessageDialog(null,
+                """
+                        "Trivia Maze" is a collaborative project developed by Team 2.
+                        Any similarities between this and any other projects, past, present, and/or future
+                        "Trivia Maze" projects is strictly coincidental,
+                        and in no way an omission that their project is superior;
+                        Because this one is better.\s"""));
+        infoMenu.add(about);
+
+        return infoMenu;
     }
     public void saveAndLoad() {
 
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,19 +1,18 @@
+package view;
+
 import javax.swing.SwingUtilities;
-import view.GameFrame;
 
 /**
- * Main driver for Trivia Maze.
+ * view.Main driver for Trivia Maze.
  * @author rick_adams.
  * @author Viktoria Dolojan.
  */
 public class Main {
     /**Private constructor.*/
     private Main() {
-
     }
-
     /**
-     * Main method.
+     * view.Main method.
      * @param thrArgs command line args.
      */
     public static void main(final String[] thrArgs) {
