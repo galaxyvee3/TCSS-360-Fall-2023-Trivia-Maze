386ca6d7a07b45b4075be4cb220656204422df99
radams42 <radams42@uw.edu>
2023-11-07 20:37:58 -0800

    SQL debugged.

--- src/view/QuestionDatabase.java
+++ src/view/QuestionDatabase.java
@@ -9,44 +9,42 @@ import org.sqlite.SQLiteDataSource;
 public class QuestionDatabase {
     private static final Logger logger = Logger.getLogger(QuestionDatabase.class.getName());
 
+    private static SQLiteDataSource ds;
+
     public QuestionDatabase() {
 
     }
-
-    public static void main(String[] args) {
-        QuestionDatabase.initializeDatabase();
-    }
-
-    private static SQLiteDataSource createDataSource() {
-        SQLiteDataSource ds = new SQLiteDataSource();
+    private static SQLiteDataSource  createDataSource() {
+        ds = new SQLiteDataSource();
         ds.setUrl("jdbc:sqlite:questions.db");
         return ds;
     }
-
-    private static void createQuestionsTable(Connection conn) throws SQLException {
+    private static void createQuestionsTable() throws SQLException {
         String query = "CREATE TABLE IF NOT EXISTS questions ( " +
-                "QUESTION TEXT NOT NULL, " +
-                "ANSWER TEXT NOT NULL )";
+                       "QUESTION TEXT NOT NULL, " +
+                       "ANSWER TEXT NOT NULL )";
 
-        try (Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate(query);
+        try (Connection conn = ds.getConnection();
+             Statement stmt = conn.createStatement()) {
+             stmt.executeUpdate(query);
         }
     }
 
-    private static void insertQuestions(Connection conn) throws SQLException {
+    private static void insertQuestions() throws SQLException {
         String query1 = "INSERT INTO questions ( QUESTION, ANSWER ) VALUES ( 'Will a giant meteor hit?', 'Fingers crossed' )";
         String query2 = "INSERT INTO questions ( QUESTION, ANSWER ) VALUES ( 'Last forever?', 'November Rain' )";
-
-        try (Statement stmt = conn.createStatement()) {
+        try (Connection conn = ds.getConnection();
+             Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(query1);
             stmt.executeUpdate(query2);
         }
     }
 
     public static void initializeDatabase() {
-        try (Connection conn = createDataSource().getConnection()) {
-            createQuestionsTable(conn);
-            insertQuestions(conn);
+        ds = createDataSource();
+        try {
+            createQuestionsTable();
+            insertQuestions();
         } catch (SQLException e) {
             logger.severe("Database initialization failed: " + e.getMessage());
             System.exit(1);
