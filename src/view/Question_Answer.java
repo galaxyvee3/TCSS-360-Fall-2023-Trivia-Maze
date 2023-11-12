13807c1efb09af75a222b27e270c52e64ce46bad
radams42 <122660178+radams42@users.noreply.github.com>
2023-11-11 22:53:10 -0800

    SQLite database.

--- src/view/Question_Answer.java
+++ src/view/Question_Answer.java
@@ -1,18 +1,20 @@
 package view;
-
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Question_Answer class for Trivia Maze.
  * @author Viktoria Dolojan
  */
 public class Question_Answer {
-    /* ArrayList of Trivia questions. */
-    private ArrayList myQuestions;
 
+    private List<Map<String, String>> myQuestions;
     public Question_Answer() {
-
+        myQuestions = new ArrayList<>();
     }
 
     public List getQuestions() {
@@ -27,4 +29,31 @@ public class Question_Answer {
     public String toString() {
         return null;
     }
-}
+
+
+        public static class TriviaQuestion {
+            private String question;
+            private String answer;
+
+            public TriviaQuestion(String question, String answer) {
+                this.question = question;
+                this.answer = answer;
+            }
+
+            public String getQuestion() {
+                return question;
+            }
+
+            public String getAnswer() {
+                return answer;
+            }
+
+            @Override
+            public String toString() {
+                return "TriviaQuestion{" +
+                        "question='" + question + '\'' +
+                        ", answer='" + answer + '\'' +
+                        '}';
+            }
+        }
+    }
