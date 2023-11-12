0db31ee2543645b35138cd2ee16c1820577e2b8f
vdolojan <airotkiv15@gmail.com>
2023-11-11 20:43:17 -0800

    Draw Doors

--- src/view/DrawMaze.java
+++ src/view/DrawMaze.java
@@ -1,7 +1,6 @@
 package view;
 
 import controller.Maze;
-import model.Room;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,8 +18,11 @@ public class DrawMaze extends JPanel implements PropertyChangeListener {
     /* The size of the maze. */
     private static final int MAZE_SIZE = 6;
 
-    /* The size of the rectangles when painting rooms. */
-    private static final int RECT_SIZE = 50;
+    /* The size of the rooms. */
+    private static final int ROOM_SIZE = 60;
+
+    /* The size of the doors. */
+    private static final int DOOR_SIZE = 30;
 
     public DrawMaze(final Maze theMaze) {
         super();
@@ -34,12 +36,27 @@ public class DrawMaze extends JPanel implements PropertyChangeListener {
         super.paintComponents(theGraphics);
         final Graphics2D g2d = (Graphics2D) theGraphics;
 
-        g2d.setPaint(Color.BLACK);
+        // draw rooms
+        g2d.setPaint(Color.DARK_GRAY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
         for (int rows = 0; rows < MAZE_SIZE; rows++) {
             for (int cols = 0; cols < MAZE_SIZE; cols++) {
-                g2d.drawRect(cols * RECT_SIZE, rows * RECT_SIZE, RECT_SIZE, RECT_SIZE);
+                g2d.fillRect(cols * ROOM_SIZE, rows * ROOM_SIZE, ROOM_SIZE, ROOM_SIZE);
+            }
+        }
+
+        // draw doors
+        g2d.setPaint(new Color(100,75,50));
+
+        for (int rows = 0; rows < MAZE_SIZE; rows++) {
+            for (int cols = 1; cols < MAZE_SIZE; cols++) {
+                g2d.fillRect(cols * ROOM_SIZE - 5, rows * ROOM_SIZE + 15, DOOR_SIZE/3, DOOR_SIZE);
+            }
+        }
+
+        for (int rows = 1; rows < MAZE_SIZE; rows++) {
+            for (int cols = 0; cols < MAZE_SIZE; cols++) {
+                g2d.fillRect(cols * ROOM_SIZE + 15, rows * ROOM_SIZE - 5, DOOR_SIZE, DOOR_SIZE/3);
             }
         }
     }
