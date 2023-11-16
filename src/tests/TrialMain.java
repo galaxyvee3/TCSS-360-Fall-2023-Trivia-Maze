4244bd57d932ec3755ee7aaae01a3a001f71e918
radams42 <122660178+radams42@users.noreply.github.com>
2023-11-15 22:58:05 -0800

    Factory work.

--- src/tests/TrialMain.java
+++ src/tests/TrialMain.java
@@ -6,11 +6,11 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import model.ClueManager;
-import model.Room;
 import view.QuestionAnswer;
 
 public class TrialMain {
 
     public static void main(String[] args) throws IOException {
-        }
+
     }
+}
